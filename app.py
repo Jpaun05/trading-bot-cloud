04bb255a4bcc83d6d79916248860d78b2eb321ca
Jpaun05 <jamie_paun@gmx.net>
2026-03-02 10:28:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,8 @@ def home():
         html += f'<img src="data:image/png;base64,{plot_url}"/>'
 
     return html
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
