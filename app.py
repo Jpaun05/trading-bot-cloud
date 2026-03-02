93b37d888168b1f93a29b24682767758ac3af183
Jpaun05 <jamie_paun@gmx.net>
2026-03-02 10:41:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import io
 import base64
